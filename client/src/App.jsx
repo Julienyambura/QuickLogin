ef312ac5b878bfeb1800b822bac81def524932ad
Julia <julianyambs18@gmail.com>
2024-11-27 17:44:36 +0300

    changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./Signup";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="Signup" element={<Signup />} />
-    </Routes>
+      <Routes>
+        <Route path="Signup" element={<Signup />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-import "../App.css";
-const Signup=()=>{
-    return(
-        <div className="sign-up-container">
-            <h1>Sign Up</h1>
-        </div>
-}
+import "../src/App.css";
+const Signup = () => {
+  return (
+    <div className="sign-up-container">
+      <form className="sign-up-form">
+        <label htmlFor="Username">Username</label>
+        <input type="text" placeholder="Username" />
+        <label htmlFor="Email">Email</label>
+        <input type="email" autoComplete="off" placeholder="Email" />
+        <label htmlFor="Password">Password</label>
+        <input type="password" placeholder="Password" />
+        <button type="submit">Signup</button>
+      </form>
+      <h1>Sign Up</h1>
+    </div>
+  );
+};
