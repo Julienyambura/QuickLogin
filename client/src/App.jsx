9b0b49a9bd9021951718d452e9a29d17bc1418e5
Julia <julianyambs18@gmail.com>
2024-12-03 08:33:41 +0300

    added lucide

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./Signup";
+import Signup from "./Components/Signup";
+import Login from "./Components/Login";
+import Home from "./Components/Home";
+import ForgotPassword from "./components/Forgotpassword";
+import ResetPassword from "./Components/ResetPassword";
+import Dashboard from "./components/Dasbboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Signup" element={<Signup />} />
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/forgotPassword" element={<ForgotPassword />}></Route>
+        <Route path="/resetPassword/:token" element={<ResetPassword />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,53 +1,64 @@
-import React from "react";
-import "../src/App.css";
+import React, { useState } from "react";
+import "../App.css";
+import Axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
 const Signup = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
-
     e.preventDefault();
-    
     Axios.post("http://localhost:3000/auth/signup", {
-        username,
-        email,
-        password,
-      }).then(response => {
-          if(response.data.status) {
-              navigate('/login')
-          }
-      }).catch(err => {
-          console.log(err)
+      username,
+      email,
+      password,
+    })
+      .then((response) => {
+        if (response.data.status) {
+          navigate("/login");
+        }
       })
-    };
+      .catch((err) => {
+        console.log(err);
+      });
   };
-
   return (
     <div className="sign-up-container">
       <form className="sign-up-form" onSubmit={handleSubmit}>
-        <label htmlFor="Username">Username</label>
-
+        <h2>Sign Up</h2>
+        <label htmlFor="username">Username:</label>
         <input
           type="text"
           placeholder="Username"
           onChange={(e) => setUsername(e.target.value)}
         />
-        <label htmlFor="Email">Email</label>
+
+        <label htmlFor="email">Email:</label>
         <input
           type="email"
           autoComplete="off"
           placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
         />
-        <label htmlFor="Password">Password</label>
+
+        <label htmlFor="password">Password:</label>
         <input
           type="password"
-          placeholder="Password"
+          placeholder="******"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Signup</button>
+
+        <button type="submit">Sign Up</button>
+        <p>
+          Have an Account? <Link to="/login">Login</Link>
+        </p>
       </form>
-      <h1>Sign Up</h1>
     </div>
   );
 };
+
+export default Signup;
